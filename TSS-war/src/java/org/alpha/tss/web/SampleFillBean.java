a891aaf0db1dfd42238639254c4f0a725f981851
Lukas Härtel <lukashaertel@gmail.com>
2016-08-26 17:12:15 +0200

    More fixes to the private stuff and added some data sample creation tools

--- TSS-war/src/java/org/alpha/tss/web/SampleFillBean.java
+++ TSS-war/src/java/org/alpha/tss/web/SampleFillBean.java
@@ -7,17 +7,17 @@ package org.alpha.tss.web;
 import java.io.Serializable;
 import java.sql.Date;
 import java.util.Currency;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import org.alpha.tss.entities.ContractStatus;
 import org.alpha.tss.entities.ContractType;
 import org.alpha.tss.entities.TimeSheetFrequency;
+import org.alpha.tss.entities.TimeSheetStatus;
 import org.alpha.tss.logic.TssLogic;
+import org.alpha.tss.logic.dao.ContractAccess;
 import org.alpha.tss.logic.dto.Contract;
-import org.alpha.tss.logic.dto.Project;
-import org.alpha.tss.logic.dto.ProjectEntry;
+import org.alpha.tss.logic.dto.TimeSheet;
 
 @RequestScoped
 @Named
@@ -27,6 +27,9 @@ public class SampleFillBean implements Serializable {
 
     @EJB
     private TssLogic tssLogic;
+
+    @EJB
+    private ContractAccess ca;
 
     public String sampleContract() {
 
@@ -47,5 +50,15 @@ public class SampleFillBean implements Serializable {
                 1);
 
         return "contract-details.xhtml?faces-redirect=true&contractId=" + contract.getId();
+    }
+
+    public String sampleTimesheet(long contractId) {
+
+        TimeSheet timeSheet = tssLogic.createTimeSheet(
+                ca.getContract(contractId),
+                TimeSheetStatus.SIGNED_BY_SUPERVISOR,
+                Date.valueOf("2017-10-10"),
+                Date.valueOf("2018-10-10"), 100);
+        return "timesheet-details.xhtml?faces-redirect=true&timeSheetId=" + timeSheet.getId();
     }
 }
