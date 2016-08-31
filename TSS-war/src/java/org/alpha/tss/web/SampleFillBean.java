16b10842b719f12253627abee604810e463ba18a
Lukas Härtel <lukashaertel@gmail.com>
2016-08-31 14:34:52 +0200

    Tests for mail service

--- TSS-war/src/java/org/alpha/tss/web/SampleFillBean.java
+++ TSS-war/src/java/org/alpha/tss/web/SampleFillBean.java
@@ -8,12 +8,18 @@ import java.io.Serializable;
 import java.sql.Date;
 import java.util.Currency;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.mail.MessagingException;
 import org.alpha.tss.entities.ContractStatus;
 import org.alpha.tss.entities.ContractType;
 import org.alpha.tss.entities.TimeSheetFrequency;
+import org.alpha.tss.logic.ReminderBean;
 import org.alpha.tss.logic.TssLogic;
 import org.alpha.tss.logic.dto.Contract;
 import org.alpha.tss.logic.dto.Project;
@@ -27,6 +33,9 @@ public class SampleFillBean implements Serializable {
 
     @EJB
     private TssLogic tssLogic;
+
+    @EJB
+    private ReminderBean reminderBean;
 
     public String sampleContract() {
 
@@ -48,10 +57,19 @@ public class SampleFillBean implements Serializable {
 
         return "contract-details.xhtml?faces-redirect=true&contractId=" + contract.getId();
     }
-    
+
     public String sampleProject() {
         Project project = tssLogic.createProject("Testprojekt");
 
         return "project-details.xhtml?faces-redirect=true&projectId=" + project.getId();
+    }
+
+    public void testSendMail() {
+        try {
+            reminderBean.collectAndSendReminders();
+        } catch (MessagingException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(ex.getMessage()));
+            Logger.getLogger(SampleFillBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
